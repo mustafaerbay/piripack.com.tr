4a9d68999c27a5e2b6d7f5dc7b8630191c877621
Mustafa Erbay <mustafaerbay365@gmail.com>
2024-09-13 15:57:48 +0300

    category listing design update

--- src/lib/Category.svelte
+++ src/lib/Category.svelte
@@ -7,15 +7,20 @@
 </script>
 
 <!-- <div> -->
-<div id="category"
-	class="rounded-md hover:rounded-3xl max-w-sm bg-white dark:bg-gray-800 dark:border-gray-700 relative"
->
-<!-- <div id="category"
+<div id="category" class="flex max-w-sm flex-col rounded-md dark:border-gray-700 dark:bg-gray-800">
+	<!-- <div
+	id="category"
+	class="relative max-w-sm rounded-md bg-white hover:rounded-3xl dark:border-gray-700 dark:bg-gray-800"
+> -->
+	<!-- <div id="category"
 	class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
 > -->
-	<a href="#">
-		<img class="rounded-md hover:rounded-3xl" style="display=inline-block;" src={url} alt="" />
-	</a>
+	<div>
+		<a href="#">
+			<img class="rounded-md object-contain" style="display=inline-block;" src={url} alt="" />
+			<!-- <img class="rounded-md hover:rounded-3xl" style="display=inline-block;" src={url} alt="" /> -->
+		</a>
+	</div>
 	<div class="p-5">
 		<a href="/contact">
 			<h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
@@ -27,13 +32,14 @@
 		</p>
 		<div class="relative">
 			<!-- <Badge rounded color="green">Green</Badge> -->
-			<a id="bottom_button" 
+			<a
+				id="bottom_button"
 				href="/contact"
-				class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 absolute right-0 bottom-0"
+				class="absolute bottom-0 right-0 inline-flex items-center rounded-lg bg-blue-700 px-3 py-2 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
 			>
 				Bilgi Al
 				<svg
-					class="rtl:rotate-180 w-3.5 h-3.5 ms-2"
+					class="ms-2 h-3.5 w-3.5 rtl:rotate-180"
 					aria-hidden="true"
 					xmlns="http://www.w3.org/2000/svg"
 					fill="none"
@@ -72,8 +78,17 @@
 		border-style: solid;
 		border-width: 1px;
 		border-color: var(--color-theme-1);
-		
+		-webkit-transition: all 2s;
+	}
 
+	/* img {
+		-webkit-transition: all 2s;
+	} */
+	#category:hover {
+		transition: all 2s;
+		/* border-radius: 25%; */
+		border-end-end-radius: 2rem;
+		background-color: var(--color-theme-3);
 	}
 	div {
 		position: relative;
