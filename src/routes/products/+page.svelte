fbb3888d53dbb63db9331bfebedd411a86d57562
Mustafa Erbay <mustafaerbay365@gmail.com>
2024-09-13 18:10:14 +0300

    TODO: update products page

--- src/routes/products/+page.svelte
+++ src/routes/products/+page.svelte
@@ -1,4 +1,6 @@
 <script>
+	// @ts-nocheck
+
 	import Category from '$lib/Category.svelte';
 	import { products } from '../../app';
 	import { categories } from '../../app';
@@ -10,7 +12,32 @@
 	{/each}
 </div>
 <!-- TODO: fix product list -->
-<div  class="flex-container relative p-4">
+<div class="relative grid flex flex-col justify-center">
+	{#each categories as category, index}
+		{@const catId = category.id}
+		<div class="solid relative m-2 flex flex-col border">
+			<div class="flex flex-col justify-center">
+				<h2 class="m-0">{category.name}</h2>
+				<h2 class="m-0">{catId}</h2>
+			</div>
+			<div class="flex-container relative flex">
+				{#each products as product}
+					{#if product.category_id == catId}
+						<div class="flex m-1 h-64 w-64 text-wrap border">
+							<h3 class="">{product.name}</h3>
+						</div>
+					{:else}
+						<div class="m-1">
+							<!-- <h3>no product exist in this category</h3>
+							<h3>{product.id}</h3> -->
+						</div>
+					{/if}
+				{/each}
+			</div>
+		</div>
+	{/each}
+</div>
+<div class="flex-container relative p-4">
 	{#each products as product}
 		<div id="products" class="p-4">
 			<div>
@@ -51,7 +78,5 @@
 		/* border-style: solid;
 		border-width: 1px;
 		border-color: var(--color-theme-1); */
-		
-
 	}
 </style>
