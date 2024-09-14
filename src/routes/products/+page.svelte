9e16a5a69d10e2b524c757f5c5116b8e3ccacac1
Mustafa Erbay <mustafaerbay365@gmail.com>
2024-09-14 14:34:29 +0300

    modal fix

--- src/routes/products/+page.svelte
+++ src/routes/products/+page.svelte
@@ -4,11 +4,13 @@
 	import Category from '$lib/Category.svelte';
 	import { products } from '../../app';
 	import { categories } from '../../app';
-	import { Button, Modal,Badge } from 'flowbite-svelte';
+	import { Button, Modal, Badge } from 'flowbite-svelte';
 	let defaultModal = false;
-	function alert_2() {
+	let modal_bind = ""
+	function alert_2(index) {
 		// alert("Hello! I am an alert box!!");
 		defaultModal = true;
+		modal_bind = products[index]
 	}
 </script>
 
@@ -31,9 +33,9 @@
 			<div
 				class="m-2 flex grid grid-cols-1 items-center gap-4 justify-self-center md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4"
 			>
-				{#each products as product}
+				{#each products as product,i}
 					{#if product.category_id == catId}
-						<Button on:click={alert_2} class="bg-0">
+						<Button on:click={alert_2(i)} class="bg-0">
 							<div class="bg-cyan relative h-72 w-64 items-center text-wrap border">
 								<!-- <div class="relative place-content-stretch place-self-stretch justify-center items-center h-72 w-64 text-wrap border"> -->
 								<img
@@ -47,54 +49,52 @@
 								</div>
 							</div>
 						</Button>
-						<Modal title={product.name} bind:open={defaultModal} autoclose outsideclose=true>
-							<div class="">
-								<p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-									{product.description} descrıptıon
-								</p>
-								<p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-									{product.long_description}
-								</p>
-								<div class="flex justify-center">
-									<p>Ürün Özellikleri</p>
-								</div>
-								<div class="flex grid grid-cols-1 xl:grid-cols-3 divide-x-4 divide-solid divide-lime-500 p-1">
-									<div>
-										<p>
-											Ebatlar
-										</p>
-										<ul>
-											{#each product.tags as item}
-											<li>
-												<Badge color="green">{item}</Badge>
-												<!-- {item} -->
-											</li>
-											{/each}
-										</ul>
-									</div>
-									<div>
-										<li><span class="icon">&#10004;</span>PET/PETMET/PE</li>
-										<li><span class="icon">&#10004;</span>PET/ALU/PE</li>
-										<li><span class="icon">&#10004;</span>PET/CPP</li>
-										<li><span class="icon">&#10004;</span>PE/EVOH/PE</li>
-										<li><span class="icon">&#10004;</span>PA/PE</li>
-										ikinci div
-									</div>
-									<div>
-										ucuncu div
-									</div>
-								</div>
-							</div>
-							<!-- <svelte:fragment slot="footer">
-								<Button on:click={() => alert('Handle "success"')}>I accept</Button>
-								<Button color="alternative">Decline</Button>
-							</svelte:fragment> -->
-						</Modal>
 					{/if}
 				{/each}
 			</div>
 		</div>
 	{/each}
+	<Modal title={modal_bind.name} bind:this={modal_bind} bind:open={defaultModal} autoclose outsideclose="true">
+		<div class="">
+			<p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+				{modal_bind.description} descrıptıon
+			</p>
+			<p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+				{modal_bind.long_description}
+			</p>
+			<div class="flex justify-center">
+				<p>Ürün Özellikleri</p>
+			</div>
+			<div
+				class="flex grid grid-cols-1 divide-x-4 divide-solid divide-lime-500 p-1 xl:grid-cols-3"
+			>
+				<div>
+					<p>Ebatlar</p>
+					<ul>
+						{#each modal_bind.tags as item}
+							<li>
+								<Badge color="green">{item}</Badge>
+								<!-- {item} -->
+							</li>
+						{/each}
+					</ul>
+				</div>
+				<div>
+					<li><span class="icon">&#10004;</span>PET/PETMET/PE</li>
+					<li><span class="icon">&#10004;</span>PET/ALU/PE</li>
+					<li><span class="icon">&#10004;</span>PET/CPP</li>
+					<li><span class="icon">&#10004;</span>PE/EVOH/PE</li>
+					<li><span class="icon">&#10004;</span>PA/PE</li>
+					ikinci div
+				</div>
+				<div>ucuncu div</div>
+			</div>
+		</div>
+		<!-- <svelte:fragment slot="footer">
+			<Button on:click={() => alert('Handle "success"')}>I accept</Button>
+			<Button color="alternative">Decline</Button>
+		</svelte:fragment> -->
+	</Modal>
 </div>
 <div class="flex-container relative mb-4 p-4">
 	{#each products as product}
